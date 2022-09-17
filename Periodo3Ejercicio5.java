5473abdc7e004b5ad5e45fcde1361c74db750b87
jimmyalfaro <113491613+jimmyalfaro@users.noreply.github.com>
2022-09-16 18:14:39 -0600

    Create Periodo3Ejercicio5.java

---
+++ Periodo3Ejercicio5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner Reader = new Scanner (System.in);
+
+    int contador;
+    int fin;
+    
+    System.out.println("Por favor ingrese el valor inicial (contador)");
+    contador = Reader.nextInt();
+
+    System.out.println("Por favor ahora ingrese el valor final");
+    fin = Reader.nextInt();
+
+    while (contador <= fin){
+      System.out.println("Valor actual de la iteracion " + contador);
+      contador++;
+    }
+  }
+}
